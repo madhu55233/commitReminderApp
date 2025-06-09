c22b2e1de43423d66a0f64f7ebac25504a386b81
Madhumitha <madhumitha@opentext.com>
2025-06-09 17:21:29 +0530

    Initial Commit

---
+++ CommitReminderApp/CommitReminderAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CommitReminderAppApp.swift
+//  CommitReminderApp
+//
+//  Created by Madhumitha on 09/06/25.
+//
+
+import SwiftUI
+
+@main
+struct CommitReminderAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
